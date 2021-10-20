d18cf48db6b12b6f091f383b1eb79117d212b061
msuero1906 <43377325+msuero1906@users.noreply.github.com>
2021-10-20 18:33:45 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./styles.css";
+
+// write your function here
+// so you just need to pass one parameter to the function
+
+function favColor (opColor, myColor){
+  if (favColor == opColor){
+    console.log();
+  } else (favColor == myColor){
+    console.log();
+  }
+}
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
